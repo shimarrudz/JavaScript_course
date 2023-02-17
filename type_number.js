bead698fa575da7cb8bd340b022fd4d421b2c739
unknown <vic.shima.vss@gmail.com>
2023-02-17 15:01:58 -0300

    curso

---
+++ type_number.js
@@ -0,0 +1,23 @@
+//number
+
+const meuNumero = 3;
+const num1 = 1;
+const num2 = 2;
+
+
+const operacaoMatematica = num1 * num2;
+
+console.log(operacaoMatematica)
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuantesemZero = .5; //==0.5
+
+const novaOperacao = num1 / pontoFlutuantesemZero
+
+console.log(novaOperacao)
+
+//NaN --> Not A Number (não é um número)
+
+var a = 0
+var b = 0
+console.log(a/b) // NaN
---
+++ type_strings.js
@@ -0,0 +1,36 @@
+/*
+const texto1 = 'Olá Mundo!'
+const texte2 = 'Olá Mundo novamente!'
+const senha = 'Surf@030403'
+
+
+//Concatenação 
+
+const citacao = 'O meu nome é '
+const meuNome = 'Victor'
+
+console.log(citacao + meuNome)
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+*/
+
+
+const senha1 = "minhaSenha123"
+console.log(senha1.length) // 13 caracteres
+
+
